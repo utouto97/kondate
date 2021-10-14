a214994c75b88835ac54156dd4a3fc65814e5215
utouto97 <utouto97@gmail.com>
2021-10-14 22:24:31 +0900

    cross-env を導入

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,10 @@
+const environment = process.env.NODE_ENV || 'development'
+const env = require(`./env/${environment}.ts`)
+
 export default {
+  router: {
+    base: env.BASE_URL
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'kondate',
